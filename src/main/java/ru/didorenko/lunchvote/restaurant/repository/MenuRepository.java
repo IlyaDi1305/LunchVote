0be3bba59d869a52e1debe6262bf187aa53d1fc3
IlyaDi1305 <116590021+IlyaDi1305@users.noreply.github.com>
2024-11-07 16:13:45 +0100

    add repository

---
+++ src/main/java/ru/didorenko/lunchvote/restaurant/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package ru.didorenko.lunchvote.restaurant.repository;
+
+import ru.didorenko.lunchvote.common.BaseRepository;
+import ru.didorenko.lunchvote.restaurant.model.Menu;
+
+public interface MenuRepository extends BaseRepository<Menu> {
+}
---
+++ src/main/java/ru/didorenko/lunchvote/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package ru.didorenko.lunchvote.restaurant.repository;
+
+import ru.didorenko.lunchvote.common.BaseRepository;
+import ru.didorenko.lunchvote.restaurant.model.Restaurant;
+
+public interface RestaurantRepository extends BaseRepository<Restaurant> {
+}
